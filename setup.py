48d336a46368024f7c05560324fa0e4e40f71062
atash <atash@LAPTOP-R2LDVLRR>
2024-09-29 12:37:00 -0500

    Update GifPlotter version to 0.1.1
    Incremented version in setup.py from 0.1.0 to 0.1.1. This indicates a minor update, likely including small improvements or bug fixes.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="GifPlotter",
-    version="0.1.0",
+    version="0.1.1",
     author="Amin Atashnezhad",
     author_email="atashnezhad1@gmail.com",
     description="A library to create and arrange plots and generate GIFs.",
